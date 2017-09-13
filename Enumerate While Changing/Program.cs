52e16dd42200191e3f6a9abfeb613656d705f77d
Andrei Mungiu <andrei_mungiu@yahoo.com>
2017-09-13 22:34:06 +0200

    Tutorial Checkpoint

--- Enumerate While Changing/Program.cs
+++ Enumerate While Changing/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Enumerate_While_Changing
 {
@@ -6,7 +8,33 @@ namespace Enumerate_While_Changing
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            AllDaysOfWeek allDays = new AllDaysOfWeek();
+            foreach (string day in allDays)
+            {
+                Console.WriteLine(day);
+            }
+        }
+
+        public class AllDaysOfWeek : IEnumerable<string>
+        {
+            public IEnumerator<string> GetEnumerator()
+            {
+                Console.WriteLine("Calling generic GetEnumerator");
+                //"yield return" is not "return"
+                //C# figures you want the compiler to write an enumerator for you
+                //so the compiler does all the work for you
+                yield return "Monday";
+                yield return "Tuesday";
+                yield return "Wednesday";
+            }
+
+            //this second method is generally obsolete
+            //inherits from old non-generic IEnumerable interface
+            //which exposes the same method which is not generic (not strongly typed)
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
         }
     }
 }
